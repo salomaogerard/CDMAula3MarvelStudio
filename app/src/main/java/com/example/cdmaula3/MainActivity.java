18b7db4cc565e3d7c16adbc2d3b0c669bf574064
Nicolas Prates <nicolasprates97@gmail.com>
2020-10-07 15:14:58 -0300

    Tela de registro concluida, intent criada

--- app/src/main/java/com/example/cdmaula3/MainActivity.java
+++ app/src/main/java/com/example/cdmaula3/MainActivity.java
@@ -2,16 +2,29 @@ package com.example.cdmaula3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView botaoCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        botaoCadastro = findViewById(R.id.register);
+            botaoCadastro.setOnClickListener(new View.OnClickListener(){
+                @Override
+                    public void onClick(View v){
+                        Intent it = new Intent(MainActivity.this, RegisterActivity.class);
+                        startActivity(it);
+                }
+        });
     }
-
-
 }
