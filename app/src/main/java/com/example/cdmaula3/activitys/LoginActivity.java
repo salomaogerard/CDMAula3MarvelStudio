3ef52c2c822ae0a609b5f8e2461bc38d5d33d483
viniciusfaitao <57546535+viniciusfaitao@users.noreply.github.com>
2020-11-04 19:01:31 -0300

    Add create account with email and password

--- app/src/main/java/com/example/cdmaula3/activitys/LoginActivity.java
+++ app/src/main/java/com/example/cdmaula3/activitys/LoginActivity.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,9 +28,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
 
-    private TextView buttonRegister, buttonSignIn, buttonSignInGoogle;
+    private EditText editText_Email, editText_Password;
+    private TextView button_register, button_LoginEmail, button_LoginGoogle, button_RecoverPassword;
 
     private GoogleSignInClient googleSignInClient;
     private FirebaseAuth firebaseAuth;
@@ -39,28 +42,17 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
-        buttonRegister = findViewById(R.id.register);
-        buttonRegister.setOnClickListener(new View.OnClickListener(){
-                @Override
-                    public void onClick(View v){
-                        Intent it = new Intent(LoginActivity.this, RegisterActivity.class);
-                        startActivity(it);
-                }
-        });
+        editText_Email = (EditText)findViewById(R.id.email);
+        editText_Password = (EditText)findViewById(R.id.password);
+        button_LoginGoogle = findViewById(R.id.botaoSignInGoogle);
+        button_LoginEmail = findViewById(R.id.botaoSignIn);
+        button_RecoverPassword = findViewById(R.id.forgetPassword);
+        button_register = findViewById(R.id.register);
 
-        buttonSignIn = findViewById(R.id.botaoSignIn);
-        buttonSignIn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent it = new Intent(LoginActivity.this, FirstPageActivity.class);
-                startActivity(it);
-            }
-        });
 
-        buttonSignInGoogle = findViewById(R.id.botaoSignInGoogle);
         firebaseAuth = FirebaseAuth.getInstance();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -70,18 +62,60 @@ public class LoginActivity extends AppCompatActivity {
 
         googleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        buttonSignInGoogle.setOnClickListener(new View.OnClickListener() {
+        button_LoginGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signIn();
+                signInWithGoogle();
             }
         });
 
+        button_LoginEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInWithEmail();
+            }
+        });
+
+        button_register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent it = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(it);
+            }
+        });
+
+        //        button_RecoverPassword.setOnClickListener(this);
 
 
     }
 
-    private void signIn(){
+    private void signInWithEmail(){
+        String email = editText_Email.getText().toString().trim();
+        String password = editText_Password.getText().toString().trim();
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(getBaseContext(), "Preencha todos os campos", Toast.LENGTH_LONG).show();
+        }else{
+            confirmLoginEmail(email, password);
+        }
+    }
+
+    private void confirmLoginEmail(String email, String password){
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent it = new Intent(LoginActivity.this, FirstPageActivity.class);
+                    startActivity(it);
+                    Toast.makeText(getBaseContext(), "Logado com sucesso", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getBaseContext(), "Erro ao logar", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void signInWithGoogle(){
         Intent signInIntent = googleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
--- app/src/main/java/com/example/cdmaula3/activitys/RegisterActivity.java
+++ app/src/main/java/com/example/cdmaula3/activitys/RegisterActivity.java
@@ -1,19 +1,82 @@
 package com.example.cdmaula3.activitys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.cdmaula3.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity  implements  View.OnClickListener{
+
+    private EditText editText_Email, editText_Password, editText_ConfirmPassword;
+    private Button button_Register, button_Cancel;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
+
+        editText_Email = (EditText)findViewById(R.id.registerEmail);
+        editText_Password = (EditText)findViewById(R.id.registerSenha);
+        editText_ConfirmPassword = (EditText)findViewById(R.id.registerConfirmarSenha);
+
+        button_Register = (Button)findViewById(R.id.botaoSignUp);
+        button_Cancel = (Button)findViewById(R.id.botaoCancel);
+
+        button_Register.setOnClickListener(this);
+
+        auth = FirebaseAuth.getInstance();
     }
 
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.botaoSignUp:
+                register();
+                break;
+        }
+    }
 
+    private void register(){
+        String email = editText_Email.getText().toString().trim();
+        String password = editText_Password.getText().toString().trim();
+        String confirmPassword = editText_ConfirmPassword.getText().toString().trim();
+
+        if(email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()){
+            Toast.makeText(getBaseContext(), "ERRO: Preencha todos os campos", Toast.LENGTH_LONG).show();
+        }else{
+            if(password.contentEquals(confirmPassword)){
+                createNewUser(email, password);
+            }else{
+                Toast.makeText(getBaseContext(), "ERRO: Senhas não coincidem", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void createNewUser(String email, String password){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent it = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(it);
+                    Toast.makeText(getBaseContext(), "Cadastro efetuado com Sucesso", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getBaseContext(), "Erro ao Cadastrar", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 }
